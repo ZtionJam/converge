5c9563d1de90f084f64d0276eec8ecea4f4207eb
ZtionJam <work@ztion.cn>
2024-06-25 14:45:15 +0800

    server init

---
+++ converge-server/src/main/java/cn/ztion/converge/server/ConvergeServerApplication.java
@@ -0,0 +1,13 @@
+package cn.ztion.converge.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvergeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConvergeServerApplication.class, args);
+    }
+
+}
